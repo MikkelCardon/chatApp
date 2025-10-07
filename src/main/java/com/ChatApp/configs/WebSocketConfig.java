1d302a62233537a994fa0e2a17bf7a1c6a5b7f4b
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-10-07 13:16:24 +0200

    SOCKET class for setting up messageBroker and STOMP endpoint

---
+++ src/main/java/com/ChatApp/configs/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.ChatApp.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // messages sent to /topic will be broadcasted
+        config.enableSimpleBroker("/topic");
+        // messages sent to /app will be routed to @MessageMapping methods
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // endpoint to connect to websocket
+        registry
+                .addEndpoint("/ws-chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
